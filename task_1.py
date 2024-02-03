d5ea5178ceaa403df617f3fd03e19a5fc641272a
Lam Tang <tangyenan@gmail.com>
2024-02-03 23:01:33 +0800

    multiple force layouts; write dot file

--- task_1.py
+++ task_1.py
@@ -1,7 +1,13 @@
+# uncomment if you didn't install OGDF globally:
+# %env OGDF_BUILD_DIR=~/ogdf/build-debug
 from ogdf_python import *
 
 cppinclude("ogdf/fileformats/GraphIO.h")
 cppinclude("ogdf/energybased/FMMMLayout.h")
+cppinclude("ogdf/energybased/FastMultipoleEmbedder.h")
+cppinclude("ogdf/energybased/MultilevelLayout.h")
+cppinclude("ogdf/energybased/SpringEmbedderGridVariant.h")
+cppinclude("ogdf/energybased/SpringEmbedderKK.h")
 
 # 创建一个图形对象
 G = ogdf.Graph()
@@ -10,13 +16,18 @@ G = ogdf.Graph()
 GA = ogdf.GraphAttributes(G, ogdf.GraphAttributes.all)
 
 # 读取GML文件
-ogdf.GraphIO.read(GA, G, "input/xlab.gml")
+ogdf.GraphIO.read(GA, G, "input/xlab_ra.gml")
 
 # 创建FMMMLayout对象
+# layout = ogdf.FastMultipoleEmbedder()
 layout = ogdf.FMMMLayout()
+# layout = ogdf.MultilevelLayout()
+# layout = ogdf.SpringEmbedderGridVariant()
+# layout = ogdf.SpringEmbedderKK()  # 只能用于连通图
+
 
 # 设置FMMMLayout的参数（例如：温度、最大迭代次数等）
-layout.unitEdgeLength(100.0)  # 设置单位边长
+# layout.unitEdgeLength(100.0)  # 设置单位边长
 # layout.setTemperature(1.0)     # 设置初始温度
 # layout.setCoolingFactor(0.8)  # 设置冷却因子
 # layout.setMaxIter(500)        # 设置最大迭代次数
@@ -31,11 +42,14 @@ for v in G.nodes:
     GA.fillColor[v] = ogdf.Color(0, 0, 0)  # 设置节点填充颜色
     GA.shape[v] = ogdf.Shape.Ellipse  # 设置节点形状为椭圆
     GA.label[v] = ""  # 隐藏标签
+    # GA.x[v] = 1.1  # 可以修改坐标
 
 # 遍历所有边，改变颜色
 for e in G.edges:
+    # GA.strokeColor[e] = ogdf.Color(178, 178, 178)  # 设置边颜色
     GA.strokeColor[e] = ogdf.Color(0, 0, 0)  # 设置边颜色
     GA.strokeWidth[e] = 0.1  # 设置节点宽度
     GA.label[e] = ""  # 隐藏边的标签
 
-ogdf.GraphIO.drawSVG(GA, "output/xlab.svg")
+ogdf.GraphIO.drawSVG(GA, "output.svg")
+ogdf.GraphIO.write(GA, "output.dot")
